42d6665807559f62d35fcc778ba85c2f8d39f233
Mouhtal Oussama <mouhtal@omnissiah.cerfacs.fr>
2024-05-28 13:48:20 +0200

    commit

--- Data_assimilation_framework/VaritionalMethods/logger.py
+++ Data_assimilation_framework/VaritionalMethods/logger.py
@@ -31,12 +31,14 @@ def plot(qudcost, max_outer, max_inner, OuterLoop = False, LineStyle = ':d',labe
 
 
 # ===================== The Problem Parameters =====================
+""" Choose n > Nt * m_t"""
 n = 300
-m_t = 40
-Nt = 2 
+m_t = 40 
+Nt = 2  
 
 # ===================== The Solvers Parameters =====================
-max_inner = 50  # max iteration for cg loops
+""" Choose max_inner =< Nt * m_t"""
+max_inner = 60  # max iteration for cg loops
 max_outer = 2   # max iteration for Gauss Newton loop
 
 
